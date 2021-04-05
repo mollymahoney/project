651d5102b3ae24151cf5e991e009049ff8d7c4f4
mollymahoney <81712297+mollymahoney@users.noreply.github.com>
2021-04-05 17:29:29 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,23 @@
-name = print("what is your name?")
+inputName = input("What is your name?")
+print(inputName)
+
+
+inputSchoolLevel = input("Are you in High School or College? ")
+print(inputSchoolLevel)
+
+
+listGrades = []
+
+for thing in range(numOfClasses):
+    classInput = int(input("What grades did you make? "))
+    listGrades.append(classInput)
+
+print(listGrades)
+
+
+
+listClass = []
+numOfClasses = int(input("How many classes are you taking? "))
+for thing in range(numOfClasses):
+    classInput = input("What classes are you taking? ")
+    print(classInput)
